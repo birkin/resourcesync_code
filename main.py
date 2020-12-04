0d558b8c6411dd25babb006bac2854c5f508c34c
juhr <justin_uhr@brown.edu>
2020-12-04 14:41:39 -0500

    added some quick testing of ResourceList()

--- main.py
+++ main.py
@@ -5,17 +5,21 @@ Usage...
 - can be called manually by cd-ing to the `resourcesync_code` directory (with virtual-environment activated) and running:
   $ python3 ./main.py
 """
-from resync import CapabilityList
+from resync import CapabilityList, ResourceList
 
-def get_capabilities():
+def test_features():
   # Read Capability List and show supported capabilities
   cl = CapabilityList()
-  test_url = 'http://localhost:8888/capabilitylist.xml'
-  cl.read(test_url)
+  capability_url = 'http://localhost:8888/capabilitylist.xml'
+  cl.read(capability_url)
   for resource in cl:
       print(f"supports {resource.capability} (at {resource.uri})")
-  
+  rl = ResourceList()
+  resource_url = 'http://localhost:8888/resourcelist.xml'
+  rl.read(resource_url)
+  for resource in rl:
+    print(resource)
 
 
 if __name__ == '__main__':
-    get_capabilities()
+    test_features()
