44cf60ee2cfa9b5152e7ee4739b6c806e6f32c94
juhr <justin_uhr@brown.edu>
2020-12-04 14:30:09 -0500

    initial code from walkthrough w/ slight tweaks

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ Usage...
 - can be called manually by cd-ing to the `resourcesync_code` directory (with virtual-environment activated) and running:
   $ python3 ./main.py
 """
+from resync import CapabilityList
 
-
-def do_something():
-    print ('hi')
+def get_capabilities():
+  # Read Capability List and show supported capabilities
+  cl = CapabilityList()
+  test_url = 'http://localhost:8888/capabilitylist.xml'
+  cl.read(test_url)
+  for resource in cl:
+      print(f"supports {resource.capability} (at {resource.uri})")
+  
 
 
 if __name__ == '__main__':
-    result = do_something()
+    get_capabilities()
