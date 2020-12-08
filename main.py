704c5e5a5757a00b9f237b22b34a15095a3cc316
birkin <birkin.diana@gmail.com>
2020-12-08 10:12:55 -0500

    adds print-statements to help view output.

--- main.py
+++ main.py
@@ -12,11 +12,16 @@ def test_features():
   cl = CapabilityList()
   capability_url = 'http://localhost:8888/capabilitylist.xml'
   cl.read(capability_url)
+  print( "\nAbout to show all capabilities..." )
   for resource in cl:
       print(f"supports {resource.capability} (at {resource.uri})")
+
+  # Read resources
+  print( "\nAbout to show contents of one of the resources..." )
   rl = ResourceList()
-  resource_url = 'http://localhost:8888/resourcelist.xml'
+  resource_url = 'http://localhost:8888/resourcelist.xml'  # this url is one of the capabilities
   rl.read(resource_url)
+
   for resource in rl:
     print(resource)
 
