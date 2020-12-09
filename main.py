d518d2536f1324b68ac9e2cf28d8145876d4d852
juhr <justin_uhr@brown.edu>
2020-12-09 09:59:49 -0500

    checking paths explicitly

--- main.py
+++ main.py
@@ -34,8 +34,17 @@ def test_features():
   print('Sub list:')
   for r in sub_rl:
     print(r)
-  d = dump.Dump(resources=sub_rl)
-  d.write(basename='/dump_test/sub_test_')
+
+  # Getting a "No file path defined error when trying to dump, so trying to check/explore that first"
+  for r in sub_rl.resources:
+    print(f'Path = {r.path}')
+
+  # Do any of them have a path defined?
+  paths = [r.path for r in rl.resources]
+  print(f'Unique paths: {set(paths)}')
+
+  # d = dump.Dump(resources=sub_rl)
+  # d.write(basename='/dump_test/sub_test_')
 
 if __name__ == '__main__':
     test_features()
