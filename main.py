d6431f3b20e29310b8eeb16156b1c7e866eff407
juhr <justin_uhr@brown.edu>
2020-12-10 10:08:07 -0500

    initial attempt to connect to POD

--- main.py
+++ main.py
@@ -8,25 +8,24 @@ Usage...
 from resync import CapabilityList, ResourceList, ResourceDump, dump
 
 def test_features():
-  # Read Capability List and show supported capabilities
-  cl = CapabilityList()
-  capability_url = 'http://localhost:8888/capabilitylist.xml'
-  cl.read(capability_url)
-  print( "\nAbout to show all capabilities..." )
-  for resource in cl:
-      print(f"supports {resource.capability} (at {resource.uri})")
+  # Not sure if there's a capabilities list, or where it is if so...
+  # cl = CapabilityList()
+  # capability_url = 'https://pod.stanford.edu/organizations/'
+  # cl.read(capability_url)
+  # print( "\nAbout to show all capabilities..." )
+  # for resource in cl:
+  #     print(f"supports {resource.capability} (at {resource.uri})")
+
   # Read Resource List and print it
   rl = ResourceList()
-  resource_url = 'http://localhost:8888/resourcelist.xml'  # this url is one of the capabilities
+  resource_url = 'https://pod.stanford.edu/organizations/normalized_resourcelist/marcxml'
   rl.read(resource_url)
 
   for resource in rl:
     print(resource)
   # Attempting to download resources, but getting an error related to one resource
-  d = dump.Dump(resources=rl)
-  d.write(basename='./dump_test/test.xml')
+  # d = dump.Dump(resources=rl)
+  # d.write(basename='./dump_test/test.xml')
 
 if __name__ == '__main__':
     test_features()
-
-    # test edit
